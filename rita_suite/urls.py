ff72818a7f0194adc7ae367a2698a14c8fb639f6
CterLQP <116781427+CterLQP@users.noreply.github.com>
2025-10-04 12:24:18 +0700

    update translate function

--- rita_suite/urls.py
+++ rita_suite/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Quay trở lại cấu trúc URL đơn giản ban đầu
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
@@ -13,3 +14,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
